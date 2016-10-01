b66447a9dc33d5c66685c1682327e10e608c79aa
Ryan Ea <helloxhi1@gmail.com>
2016-10-01 16:18:21 -0700

    Rewrote own code to repurpose buttons to pause and skip if needed as well as change in layout

--- app/src/main/java/davidchan/pocketchef/CookingProgressActivity.java
+++ app/src/main/java/davidchan/pocketchef/CookingProgressActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,8 +16,8 @@ import java.util.ArrayList;
 //
 public class CookingProgressActivity extends Activity{
     int i=0;
-    Button timeButton;
-    Button Advance;
+    Button skipStep;
+    Button pauseButton;
     String recipeName;
     TextView steps;
     CountDownTimer timePerStep;
@@ -28,14 +27,14 @@ public class CookingProgressActivity extends Activity{
     long s= 1000;
     long M=0, S=10;
     long totalTime;
-
+    boolean pause=true;
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.activity_cooking_progress);
         Intent received = getIntent();
         timer = (TextView) findViewById(R.id.timer);
-        timeButton = (Button) findViewById(R.id.addTime);
-        Advance = (Button) findViewById(R.id.nextStep);
+        skipStep = (Button) findViewById(R.id.skipStep);
+        pauseButton = (Button) findViewById(R.id.pauseStep);
 
         totalTime= m*M + S*s;
 
@@ -45,71 +44,42 @@ public class CookingProgressActivity extends Activity{
         recipeName = recipe.getString("recipeName");
         ingredientList =recipe.getStringArrayList("ingredients");
         steps = (TextView) findViewById(R.id.Step);
-        steps.setText(recipeName);
 
-        timeButton.setOnClickListener(new View.OnClickListener() {
+        totalTime= m*M + S*s;
+        steps.setText(recipeName+ "\n" + ingredientList.get(i));
+
+        skipStep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 timePerStep.cancel();
-                Spinner getIt = (Spinner) findViewById(R.id.intervals);
-                long addTime;
-                int convert=getIt.getSelectedItemPosition();
-                switch (convert){
-                    case 0: addTime = 10 * s;
-                        break;
-                    case 1: addTime = 30 * s;
-                        break;
-                    case 2: addTime = m;
-                        break;
-                    case 3: addTime = 3 * m;
-                        break;
-                    case 4:addTime = 5 * m;
-                        break;
-                    case 5: addTime = 10 * m;
-                        break;
-                    case 6: addTime = 30 * m;
-                        break;
-                    case 7: addTime = 60 * m;
-                        break;
-                    default: addTime = 3* m;
-                        break;
+                timer.setText("0:00");
+                if(i<ingredientList.size()){
+                    steps.setText(recipeName+ "\n"+ingredientList.get(i));
+                    totalTime= m*M + s*S;
+                    pause=true;
+                    pauseButton.setText("Start");
+                    timePerStep=timePerStep(totalTime);
+                    i++;
                 }
-                if((totalTime + addTime) >= 60*m){
-                    totalTime = 59*m + 59*s;
+                if(i >= ingredientList.size()){
+                    pauseButton.setText("Done!");
+                    steps.setText("Done!");
                 }
-                else
-                    totalTime += addTime;
-                if(steps.getText()=="DONE!"){
-                    timePerStep = timePerStep(0);
-                }
-                else {
-                    timePerStep = timePerStep(totalTime);
-                }
-                timePerStep.start();
             }
         });
-
-        timePerStep=timePerStep(totalTime);
-        Advance.setOnClickListener(new View.OnClickListener() {
+        pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                steps.setText(recipeName+"HA!");
-                if(i == 0){
-                    steps.setText(recipeName + "\n" + ingredientList.get(i));
-                    timePerStep.start();
-                    i++;
-                    Advance.setText("Next");
-                } else if(i <ingredientList.size()){
+                if(!pause){
                     timePerStep.cancel();
-                    timePerStep.onTick(M);
+                    pause=!pause;
+                    pauseButton.setText("Start");
+                }
+                else {
+                    timePerStep=timePerStep(totalTime);
                     timePerStep.start();
-                    steps.setText(recipeName+ "\n" + ingredientList.get(i));
-                    i++;
-                } else if(i == ingredientList.size()){
-                    timePerStep.cancel();
-                    timePerStep.onFinish();
-                    steps.setText("DONE!");
-                    Advance.setText("DONE!");
+                    pauseButton.setText("Stop");
+                    pause = !pause;
                 }
             }
         });
@@ -120,23 +90,23 @@ public class CookingProgressActivity extends Activity{
             public void onTick(long millisUntilFinished) {
                 long min = (millisUntilFinished / 1000) / 60;
                 long sec = (millisUntilFinished / 1000) % 60;
+                totalTime=millisUntilFinished;
                 timer.setText(min + ":" + ((sec <10) ? "0"+ sec : sec));
             }
             @Override
             public void onFinish() {
-                timer.setText("00:00");
-                if(i <ingredientList.size()){
-                    steps.setText(recipeName+ "\n" + ingredientList.get(i));
+                timer.setText("0:00");
+                if(i<ingredientList.size()){
+                    steps.setText(recipeName+ "\n"+ingredientList.get(i));
+                    totalTime= m*M + s*S;
+                    pause=true;
+                    pauseButton.setText("Start");
+                    timePerStep=timePerStep(totalTime);
                     i++;
-                    timePerStep.cancel();
-                    timePerStep.onTick(M);
-                    timePerStep.start();
-
-                } else if(i >= ingredientList.size()){
-                    timePerStep.cancel();
-                    timer.setText("00:00");
-                    steps.setText("DONE!");
-                    Advance.setText("DONE!");
+                }
+                if(i >= ingredientList.size()){
+                    pauseButton.setText("Done!");
+                    steps.setText("Done!");
                 }
             }
         };
