be82b3f26f1ae7a47f6cd77aeab4dbd910aee1ce
Kenneth Ung <knnthung@gmail.com>
2016-10-01 10:26:44 -0700

    Fixed errors and cleaned up code

--- app/src/main/java/davidchan/pocketchef/AddRecipe.java
+++ app/src/main/java/davidchan/pocketchef/AddRecipe.java
@@ -2,6 +2,11 @@ package davidchan.pocketchef;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class AddRecipe extends AppCompatActivity {
 
@@ -9,5 +14,42 @@ public class AddRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
+        boolean AddIngredient = false;
+
+        Button button3 = (Button) findViewById(R.id.button);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText Ingredient = (EditText) findViewById(R.id.ingredient);
+                ArrayList<String> IngredientList = new ArrayList<String>();
+                IngredientList.add(Ingredient.getText().toString());
+            }
+        });
+
+        while(AddIngredient)
+        {
+            Button button2=(Button)findViewById(R.id.button);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    EditText Ingredient = (EditText) findViewById(R.id.ingredient);
+                    ArrayList<String> IngredientList = new ArrayList<String>();
+                    IngredientList.add(Ingredient.getText().toString());
+                }
+            });
+
+
+
+
+            Button button=(Button)findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    boolean AddIngredient = false;
+                }
+            });
+        }
     }
 }
