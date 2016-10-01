80953fca8c3f00cab7232758f632d03b923fb30a
DavidKhmerChan <DavidKhmerChan@gmail.com>
2016-10-01 10:50:05 -0700

    added new instruction activity

--- app/src/main/java/davidchan/pocketchef/GetIngredientsActivity.java
+++ app/src/main/java/davidchan/pocketchef/GetIngredientsActivity.java
@@ -1,6 +1,7 @@
 package davidchan.pocketchef;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,38 +16,34 @@ import java.util.ArrayList;
 
 public class GetIngredientsActivity extends Activity
 {
+    ArrayList<String> IngredientList = new ArrayList<String>();
     protected void onCreate(Bundle saveInstantBundle)
     {
         super.onCreate(saveInstantBundle);
         setContentView(R.layout.activity_ingredients);
 
-        boolean AddIngredient = true;
-
-        while(AddIngredient)
-        {
-            Button button2=(Button)findViewById(R.id.button2);
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    EditText Ingredient = (EditText) findViewById(R.id.ingredient);
-                    ArrayList<String> IngredientList = new ArrayList<String>();
-                    IngredientList.add(Ingredient.getText().toString());
-                }
-            });
 
 
-
-
-            Button button=(Button)findViewById(R.id.button);
+            Button button=(Button)findViewById(R.id.button2);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
                 {
-                    boolean AddIngredient = false;
+                    EditText Ingredient = (EditText) findViewById(R.id.ingredient);
+                    IngredientList.add(Ingredient.getText().toString());
                 }
             });
-        }
+
+            Button button2=(Button)findViewById(R.id.button);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    Intent returnaction= new Intent(v.getContext(), MainActivity.class);
+                    startActivity(returnaction);
+
+                }
+            });
 
 
 
