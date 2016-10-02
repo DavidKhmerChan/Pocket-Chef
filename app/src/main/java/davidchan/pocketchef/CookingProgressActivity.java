3832ab1227f51e4a94911f8c13a0222b82f25baa
David Van <David@DavidVan.net>
2016-10-01 19:38:21 -0700

    Fixed Activity choices.

--- app/src/main/java/davidchan/pocketchef/CookingProgressActivity.java
+++ app/src/main/java/davidchan/pocketchef/CookingProgressActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,8 +14,8 @@ import java.util.ArrayList;
 /**
  * Created by Ryan on 10/1/2016.
  */
-//
-public class CookingProgressActivity extends Activity{
+
+public class CookingProgressActivity extends AppCompatActivity {
     int i=0;
     Button skipStep;
     Button pauseButton;
